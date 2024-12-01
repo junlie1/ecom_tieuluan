803c9eedca0f5cf7b2e96b3daf81fc9660ba768c
junlie1 <nlt24304@gmail.com>
2024-12-01 20:53:17 +0700

    xóa

--- src/contant.js
+++
@@ -1,9 +0,0 @@
-export const orderContant = {
-    delivery: {
-        FAST: 'FAST',
-        gojek: 'GO_JEK',
-    },
-    payment: {
-        later_money: 'Thanh toán bằng tiền mặt khi nhận hàng',
-    }
-}
